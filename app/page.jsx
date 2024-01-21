75faea7c2450a68f4253a72978c4136595548c0d
Juan Romero <80675013+Juanies@users.noreply.github.com>
2024-01-21 16:05:21 +0100

    fix import

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,3 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 import  Home from '@/components/pages/Home'
 export default function Page() {
   return (
