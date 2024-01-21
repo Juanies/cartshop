f547fbcfc7bdbc8f5c62342bcdfb4ef9ab38e89f
Juan Romero <80675013+Juanies@users.noreply.github.com>
2024-01-21 16:24:07 +0100

    Delete components/subcomponents directory

--- components/subcomponents/ButtonA.jsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function ButtonA ({url, name}) {
-    return (
-      <li className='hover:bg-slate-500 hover:scale-110 transition-all rounded-md p-3 '>
-        <Link href={url}>{name}</Link>
-      </li>
-    );
-  }
