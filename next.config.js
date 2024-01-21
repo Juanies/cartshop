306e307b78c62a4b7901c4fe1f2c0b340e6b7344
Juan Romero <80675013+Juanies@users.noreply.github.com>
2024-01-21 13:12:22 +0100

    Config Img Path

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'fakestoreapi.com',
+          port: '',
+          pathname: '/img/**',
+        },
+      ],
+    },
+  }
