70f702556b0234b4d2fb64459f2a93b975d3d3f4
Juan Romero <80675013+Juanies@users.noreply.github.com>
2024-01-21 15:46:36 +0100

    Add files via upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,9 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} >
         <Header></Header>
-        {children}
+        {children }
       </body>
     </html>
   );
